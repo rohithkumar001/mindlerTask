0f53df66a2412297ed3052dc80c8a6f7e0e4c043
Rohith Kumar <rohithklv@gmail.com>
2021-05-21 22:29:46 +0530

    Added table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { GridJsAngularModule } from 'gridjs-angular';
 
 // used to create fake backend
 import { fakeBackendProvider } from './_helpers';
@@ -10,20 +11,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { AppComponent } from './app.component';
 import { AlertComponent } from './_components';
-import { HomeComponent } from './home';
+import { HomeComponent } from './home';;
+import { MytableComponent } from './mytable/mytable.component'
 
 @NgModule({
     imports: [
         BrowserModule,
         ReactiveFormsModule,
         HttpClientModule,
-        AppRoutingModule
-    ],
+        AppRoutingModule,
+        GridJsAngularModule,
+        ],
     declarations: [
         AppComponent,
         AlertComponent,
-        HomeComponent
-    ],
+        HomeComponent,
+        MytableComponent
+       ],
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
